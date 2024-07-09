d740acedf9785df3ca7cc6ef6836d11c3083b0fb
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-07-09 09:24:55 +0000

    collect staticfiles and change name in all files

--- holidai/urls.py
+++ holidai/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hello_world import views as index_views
+from helloworld import views as index_views
 
 urlpatterns = [
     path('', index_views.index, name='index'),
