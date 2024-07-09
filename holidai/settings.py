5607f839860780d0b4009a929ded9834c27ec1ff
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-07-09 11:50:31 +0000

    fixed bugs with appname

--- holidai/settings.py
+++ holidai/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "helloworld/static",
+    os.path.join(BASE_DIR, 'helloworld', 'static'), 
 ]
 
 # Default primary key field type
